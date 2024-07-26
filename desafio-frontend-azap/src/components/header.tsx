6f8eada5f7afeedee04588c77367e010c1913f01
unknown <arthurfelaco707@gmail.com>
2024-07-26 15:40:16 -0300

    feat: add input in header

--- desafio-frontend-azap/src/components/header.tsx
+++ desafio-frontend-azap/src/components/header.tsx
@@ -1,7 +1,38 @@
+"use client"
+
+import { useState } from "react"
+
 export const Header = () => {
+    const [inputValue, setInputValue] = useState<string>('');
+
     return (
-        <header>
-            teste
+        <header className="flex items-center pl-10 absolute top-0 right-0 w-full h-20 bg-slate-200 shadow-md">
+            <div className="flex justify-start items-center h-full">
+                <div className="h-1/2 relative text-center">
+                    <input
+                        className="h-full p-2"
+                        value={inputValue}
+                        type="search"
+                        autoComplete="off"
+                        autoCorrect="off"
+                        autoCapitalize="off"
+                        spellCheck="false"
+                        onChange={(e) => setInputValue(e.target.value)}
+                        placeholder=""
+                    />
+                    <label className="absolute left-2 top-2">
+                        Pesquisar
+                    </label>
+                </div>
+
+                <button
+                    className=""
+                    type="submit"
+                // onClick={handleSubmit}
+                >
+                    {/* <SearchIcon /> */}
+                </button>
+            </div>
         </header>
     )
 }
