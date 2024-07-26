a5cebe3c5f5b5a183fc59c744f9810014857b951
unknown <arthurfelaco707@gmail.com>
2024-07-26 15:40:49 -0300

    feat: change hero list and card design

--- desafio-frontend-azap/src/components/hero-card.tsx
+++ desafio-frontend-azap/src/components/hero-card.tsx
@@ -1,10 +1,25 @@
 import { Hero } from "@/types/hero";
+import Image from "next/image";
 
 export const HeroCard = ({ heroData }: { heroData: Hero }) => {
 
     return (
-        <>
-            {heroData.id}
-        </>
+        <li className="flex items-center justify-center self-center w-80 mx-auto h-full cursor-pointer">
+            <Image
+                className="shape--blob rounded-l-xl h-full"
+                alt={heroData.name}
+                src={heroData.images.lg}
+                width={200}
+                height={200}
+                quality={100}
+                loading="lazy"
+                draggable={false}
+            />
+            <div className="h-full w-1/2 flex bg-foreground justify-center items-center">
+                <h3 className="antialiased text-white text-center items-center">
+                    {heroData.name}
+                </h3>
+            </div>
+        </li>
     )
 }
